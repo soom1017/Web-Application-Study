94d3046da106c884292de8742dc2b74c0860ef73
soomin <77096986+soom1017@users.noreply.github.com>
2021-01-10 20:01:34 +0900

    added security funcs

--- temp2/index.php
+++ temp2/index.php
@@ -12,14 +12,14 @@
   <body id="target">
     <header>
       <img src=https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png alt="생활코딩"/>
-      <h1><a href="http://localhost/temp/index.php">JavaScript</a></h1>
+      <h1><a href="http://localhost/temp2/index.php">JavaScript</a></h1>
     </header>
     <nav>
       <ol>
         <?php
           while($row = mysqli_fetch_assoc($result))
           {
-            echo '<li><a href="http://localhost/temp2/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
+            echo '<li><a href="http://localhost/temp2/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
             echo "<br>";
           }
         ?>
@@ -37,11 +37,12 @@
         if(empty($_GET['id']) == false) 
         {
           $sql = 'SELECT * FROM topic WHERE id='.$_GET['id'];
-          $result = mysqli_query($conn, $sql);
+          $result = mysqli_query($conn, $sql); //mysqli_real_escape_string(): 보안 위협 특수문자 사용 방지(sql injection) 
           $row = mysqli_fetch_assoc($result);
-          echo '<h2>'.$row['title'].'</h2>';
-          echo $row['description'];
-        }
+          echo '<h2>'.htmlspecialchars($row['title']).'</h2>'; //htmlspecialchars(): php에서 보안 위협 태그 방지(xss)
+          echo '<p>'.htmlspecialchars($row['author']).'</p>';
+          echo strip_tags($row['description'], '<a><h1><h2><h3><ul><ol><li>'); //strip_tags(): php에서 보안 위협 태그 (선택적) 방지(xss)
+        } 
         
       ?>
     </article>
